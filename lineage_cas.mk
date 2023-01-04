2432edaefa3352d362ced81155fd61b48786d8dc
Julius Bairaktaris <julius@bairaktaris.de>
2023-01-04 20:18:40 +0100

    Update Build Fingerprint.
    Signed-off-by: Julius Bairaktaris <julius@bairaktaris.de>

--- lineage_cas.mk
+++ lineage_cas.mk
@@ -11,33 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
-# Inherit from the lineageOS configuration.
-$(call inherit-product, vendor/lineage/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# GApps
-$(call inherit-product-if-exists, vendor/gapps/core/config.mk)
-WITH_GMS := true
-TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_NAME := lineage_cas
 PRODUCT_DEVICE := cas
+PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
-PRODUCT_MANUFACTURER := Xiaomi
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.6.0.SJJCNXM:user/release-keys
-
-# chipset flag enclose var with "" if more than one
-# friendly tip: builders can use init_xxx.cpp as workaround for spacing
-# e.g. property_override("ro.rice.chipset", "Snapdragon 870 5G");
 RICE_CHIPSET := "Snapdragon 865 5G"
 
-# chipset flag enclose var with "" if more than one
-# friendly tip: builders can use init_xxx.cpp as workaround for spacing
-# e.g. property_override("ro.rice.maintainer", "get riced");
-RICE_MAINTAINER := zinger
+
+# riceDroid Flags
 
 # Sushi Bootanimation (only 720/1080/1440 supported. if not defined, bootanimation is google bootanimation)
 SUSHI_BOOTANIMATION := 1080
@@ -51,10 +41,15 @@ TARGET_ENABLE_BLUR := true
 # UDFPS ICONS/ANIMATIONS
 TARGET_HAS_UDFPS := true
 
-# Allow usage of custom binary linker (LD), default is false
-TARGET_KERNEL_OPTIONAL_LD := false
+# Spoof build description/fingerprint as pixel device
+TARGET_USE_PIXEL_FINGERPRINT := true
 
-TARGET_USE_PIXEL_FINGERPRINT := false
+# GMS build flags, if none were defined the package build type will be AOSP (default: false)
+WITH_GMS := true
+
+# Customized GMS Flags 
+
+# Opt out of google dialer support, compiler will build aosp dialer,
 TARGET_OPTOUT_GOOGLE_TELEPHONY := false
 
 # Quick Tap 
@@ -62,3 +57,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 
 #Face unlock 
 TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Maintainer Stuff
+RICE_MAINTAINER := zinger & starraiderx
+RICE_OFFICIAL := false
