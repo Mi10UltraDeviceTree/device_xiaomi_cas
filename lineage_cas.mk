ebe881284be99d27d21c044610db0512e9ccc8c1
starraiderx <33199500+starraiderx@users.noreply.github.com>
2023-02-09 22:01:59 +0000

    Bump to latest rice

--- lineage_cas.mk
+++ lineage_cas.mk
@@ -23,8 +23,7 @@ PRODUCT_MODEL := M2007J1SC
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # riceDroid Flags
-RICE_CHIPSET := Snapdragon-865-5G
-RICE_OFFICIAL := false
+RICE_CHIPSET := "Snapdragon 865 5G"
 
 # Sushi Bootanimation (only 720/1080/1440 supported. if not defined, bootanimation is google bootanimation)
 SUSHI_BOOTANIMATION := 1080
@@ -39,16 +38,23 @@ TARGET_ENABLE_BLUR := true
 TARGET_HAS_UDFPS := true
 
 # Spoof build description/fingerprint as pixel device
-TARGET_USE_PIXEL_FINGERPRINT := false
+TARGET_USE_PIXEL_FINGERPRINT := true
 
 # GMS build flags, if none were defined the package build type will be AOSP (default: false)
 WITH_GMS := true
 
 # Customized GMS Flags 
-TARGET_GAPPS_ARCH := arm64
+# WITH_GMS flag is required
 
-# Opt out of google dialer support, compiler will build aosp dialer,
-TARGET_OPTOUT_GOOGLE_TELEPHONY := false
+# Wether to use google (true) or AOSP (false) telephony package bundle. (defaults: false for gms core, true for pixel builds)
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+# Compiler will only build GMS playstore services, its dependencies, and Gboard app.
+# package type will change from PIXEL/GMS -> CORE
+TARGET_CORE_GMS := false
+
+# extra flag under TARGET_CORE_GMS
+TARGET_CORE_GMS_EXTRAS := false
 
 # Quick Tap 
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -57,4 +63,4 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Maintainer Stuff
-RICE_MAINTAINER := zinger&starraiderx
+RICE_MAINTAINER := "zinger&starraiderx"
