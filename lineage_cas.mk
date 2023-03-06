7f3c92847ad2b55ab213fb6a07c1e10a51259aab
Mahmoud Nabeel <mnitpro@gmail.com>
2023-03-06 22:07:45 +0300

    cas: update Rice flags

--- lineage_cas.mk
+++ lineage_cas.mk
@@ -17,17 +17,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # chipset flag enclose var with "" if more than one
 # friendly tip: builders can use init_xxx.cpp as workaround for spacing
 # e.g. property_override("ro.rice.chipset", "Snapdragon 870 5G");
-RICE_CHIPSET := "Snapdragon 865 5G"
+RICE_CHIPSET := "Snapdragon-865-5G"
 
 # chipset flag enclose var with "" if more than one
 # friendly tip: builders can use init_xxx.cpp as workaround for spacing
 # e.g. property_override("ro.rice.maintainer", "get riced");
 RICE_MAINTAINER := "zinger&starraiderx"
-
-# chipset flag enclose var with "" if more than one
-# this will reflect on build/display version, a firmware package/zip name 
-# e.g. riceDroid-7.0-COMMUNITY-device-AOSP.zip - AOSP is the default package type, WITH_GMS will override the package type to PIXEL
-RICE_PACKAGE_TYPE := "VANILLA AOSP"
 
 # Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
 SUSHI_BOOTANIMATION := 1080
@@ -48,7 +43,7 @@ TARGET_HAS_UDFPS := true
 TARGET_KERNEL_OPTIONAL_LD := false
 
 # Spoof build description/fingerprint as pixel device
-TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_USE_PIXEL_FINGERPRINT := false
 
 # GMS build flags, if none were defined the package build type will be AOSP (default: false)
 WITH_GMS := true
