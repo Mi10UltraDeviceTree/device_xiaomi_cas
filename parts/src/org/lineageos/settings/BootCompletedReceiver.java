951a2155792c115f240a22453d58d82040c24e79
Zinger <mnitpro@gmail.com>
2023-07-13 19:30:12 +0300

    Parts: Add dynamic thermal profile implementation

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,6 +25,7 @@ import org.lineageos.settings.refreshrate.RefreshUtils;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -37,5 +38,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
 		RefreshUtils.startService(context);
+        ThermalUtils.startService(context);
     }
 }
