7de149426d26a541cdfef002d74b6eed3e1ed297
Zinger <mnitpro@gmail.com>
2023-07-18 20:49:26 +0300

    Prebuilts: Update [check description]
    - DuckDuckGo: Updated to latest build [20230718]
     - SimpleCalendar: Updated to latest build [20230716]
     - SimpleGallery: Updated to latest build [20230705]
     - AdAway: Updated to latest build [20230620]
     - Lawnicons: Updated to latest build [20230615]

---
+++ prebuilts/CleanSpec.mk
@@ -0,0 +1,6 @@
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/DuckDuckGo)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Lawnicons)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/SimpleGallery)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/SimpleCalender)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/stats)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/WeatherIcons)
