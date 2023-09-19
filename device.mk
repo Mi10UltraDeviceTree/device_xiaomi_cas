bc70b90012cb9e230a1c2904b85cc63711afa80a
Julius Bairaktaris <julius@bairaktaris.de>
2023-09-19 16:13:54 +0200

    alpha-ify
    Signed-off-by: Julius Bairaktaris <julius@bairaktaris.de>

--- device.mk
+++ device.mk
@@ -106,7 +106,7 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libsndmonitor \
-    libtinycompress \
+    libtinycompress \ 
     libvolumelistener
 
 PRODUCT_COPY_FILES += \
@@ -392,6 +392,9 @@ PRODUCT_PACKAGES += \
 
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Telephony
 PRODUCT_PACKAGES += \
